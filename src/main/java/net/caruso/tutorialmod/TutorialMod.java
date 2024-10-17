0e6fc2cd4cedef34e15fb90b95d062845c9c513d
antc3519 <anth3619@gmail.com>
2024-10-17 02:29:22 -0400

    Added custom items (Pink Garnet & Raw Pink Garnet)

--- src/main/java/net/caruso/tutorialmod/TutorialMod.java
+++ src/main/java/net/caruso/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.caruso.tutorialmod;
 
+import net.caruso.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
