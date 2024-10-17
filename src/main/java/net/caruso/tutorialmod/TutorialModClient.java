53893a1ec371fda8c8fd2caa0657c3bf659de155
antc3519 <anth3619@gmail.com>
2024-10-17 02:02:41 -0400

    Initial commit

---
+++ src/main/java/net/caruso/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.caruso.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
